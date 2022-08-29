7727835e32a8783ce9ee407998e9602e01d53f05
unknown <tfb1223@gmail.com>
2022-08-28 20:47:37 -0500

    started the js

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,18 @@
-// Assignment code here
+function checkNumber() {
+  //debugger;
+  // Ask for password length
+  var number = window.prompt("How many characters will your password be? 8 characters or higher only.");
+  //var length = parseInt(number);
+  if (!number || number < 8) {
+    alert("I need a bigger number than that to generate your password.");
+  }
+  
+  checkNumber();
+}
+//console.log(length);
+//console.log(number);
 
-
+/*
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +27,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+*/
