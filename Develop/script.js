3abbd8ca1b4a4d5f9eb8f8b2bbb538493c8ad1a7
unknown <tfb1223@gmail.com>
2022-09-01 16:36:50 -0500

    random characters worked out

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,65 @@
-var alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var beta = "abcdifghijklmnopqrstuvwxyz";
-var numbers = "0123456789";
-var special = "!@#$%^&*_-+=";
+var options = [
+  alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  beta = "abcdifghijklmnopqrstuvwxyz",
+  numbers = "0123456789",
+  special = "!@#$%^&*_-+=",
+];
 
 function generatePassword() {
   // Ask for password length
   var number = prompt("How many characters will your password be? 8 characters or higher only.");
   if (!number) {
     alert("Please enter a number. :)");
-   generatePassword();
+    generatePassword();
   } else if (number < 8) {
     alert("I need a bigger number than that to generate your password.");
-   generatePassword();
+    generatePassword();
   }
   var length = parseInt(number);
 
   // Questions to set parameters of the password
   var charUp = confirm("Do you need an Uppercase letter?")
-  if (true) {
-    charUp = alpha;
+  if (true){
+    for (i =0; i < length; i++) {
+      charUp += alpha.charAt(Math.floor(Math.random() * alpha.length));
+    }
+  }
+  else {
+    charUp = "";
   };
   var charLow = confirm("Do you need a Lowercase letter?")
   if (true) {
-    charLow = beta;
-  };
+    for (i = 0; i < length; i++) {
+      charLow += beta.charAt(Math.floor(Math.random() * beta.length));
+    }
+  }
+  else {
+    charLow = "";
+  }
   var charSpc = confirm("Any special characters required?")
   if (true) {
-    charSpc = special;
-  };
+    for (i=0; i<length; i++) {
+      charSpc += special.charAt(Math.floor(Math.random() * special.length));
+    }
+  }
+  else {
+    charSpc = "";
+  }
   var charNum = confirm("Final parameter is there any numbers needed?")
   if (true) {
-    charNum = numbers;
-  };
-  
-  for (i = 0; i < length; i++){
-
+   for (i=0; i<length; i++) {
+    charNum += numbers.charAt(Math.floor(Math.random() * numbers.length));
+   }
   }
+  else {
+    charNum = "";
+  }
+
+  // This is where we take all given answers to generate your password
+  
+    var randPass = charUp + charLow + charSpc + charNum
+    console.log(randPass);
+  
 };
 
 
